ce64d334c20ba7b102350a5ad12efbf7b2567d16
OxwardInc <97251730+OxwardInc@users.noreply.github.com>
2023-09-02 08:50:21 +0100

    Add files via upload

---
+++ Industrial-Training/Class/Week 2/First.py
@@ -0,0 +1,6 @@
+a=30
+b=12
+c=a+b
+print(c)
+d = a - b
+print(d)
---
+++ Industrial-Training/Class/Week 2/Year of birth.py
@@ -0,0 +1,8 @@
+# A program to output a user yesr of birth
+
+inp = input("Enter your age: ")
+
+data = int(inp)
+result = 2023 - data
+
+print("You were born in the year ", result)
